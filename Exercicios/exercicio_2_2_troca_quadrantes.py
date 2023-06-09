781bb8e0a1e7502144c2f50f30688c840e695920
Igor Dias <igorverissimo02@hotmail.com>
2023-06-09 17:08:00 -0300

    ajuste no range do for

--- Exercicios/exercicio_2_2_troca_quadrantes.py
+++ Exercicios/exercicio_2_2_troca_quadrantes.py
@@ -1,15 +1,18 @@
 import cv2
-
+import numpy as np
 img = cv2.imread("imagens/biel.png", cv2.IMREAD_COLOR)
 img2 = img.copy() 
+linhas = np.shape(img)[0]
+colunas = np.shape(img)[1]
 
+centro =  linhas//2
 #tracando os quadrantes da imagem
-for x in range(128, 256):
-    for y in range(128, 256):
-        img2[x-128][y-128]=img[x][y]    # primeiro quadraadnte recebe o quarto
-        img2[x][y] = img[x-128][y-128]  # quarto quadrante recebe o primeiro
-        img2[x-128][y] = img[x][y-128]  # segundo quadrante recebo o terceiro
-        img2[x][y-128] = img[x-128][y]  # terceiro quadrante recebo o segundo
+for x in range(centro, linhas):
+    for y in range(centro, colunas):
+        img2[x-centro][y-centro]=img[x][y]    # primeiro quadraadnte recebe o quarto
+        img2[x][y] = img[x-centro][y-centro]  # quarto quadrante recebe o primeiro
+        img2[x-centro][y] = img[x][y-centro]  # segundo quadrante recebo o terceiro
+        img2[x][y-centro] = img[x-centro][y]  # terceiro quadrante recebo o segundo
 
 #exibindo imagem
 cv2.namedWindow("Exercicio 2.2 Trocando os quadrantes", cv2.WINDOW_AUTOSIZE)
